55af89190421004640049d358d152d37947d4bfa
Veer Single <veerafridi030027@gmail.com>
2025-11-15 20:24:40 +0000

    Try fixing this error: `Console Error: [2025-11-15T20:23:51.065Z] @fireb

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -1,3 +1,5 @@
+'use client';
+
 import { initializeApp, getApps, getApp, type FirebaseApp } from 'firebase/app';
 import { getAuth, type Auth } from 'firebase/auth';
 import { getFirestore, initializeFirestore, persistentLocalCache, persistentMultipleTabManager, type Firestore } from 'firebase/firestore';
